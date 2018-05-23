2469bbde57d2a79ac4d46ed8f34e8dd6b80ea7e4
Yevgeniy Zelepukin <w.dizel@gmail.com>
2018-05-23 18:27:34 +0300

    sto

---
+++ extra_addons/sto_customer/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'STO Customers / Cars',
+    'version': '11.0.1.0.0',
+    'category': 'Fleet service',
+    'license': 'AGPL-3',
+    'author': 'Simbioz',
+    'maintainer': 'Simbioz',
+    'website': 'http://www.simbioz.ua',
+    'depends': ['base', 'fleet'],
+    'data': ['views/customer.xml',
+             'views/menus.xml',],
+    'installable': True,
+    'application': True,
+    'sequence': 0,
+}
