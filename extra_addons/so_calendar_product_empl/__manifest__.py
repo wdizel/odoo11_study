3ee29fe54157b42f5195ed377c0d8e82d1a612d0
wdizel <w.dizel@gmail.com>
2018-10-03 11:41:35 +0300

    234

--- extra_addons/so_calendar_product_empl/__manifest__.py
+++ extra_addons/so_calendar_product_empl/__manifest__.py
@@ -4,6 +4,15 @@
     'name': 'Custom Calendar SO Tuning',
     'author':   'Alexa',
     'maintainer': 'Alexa',
+    'description': """
+    done - в товарі (product.product) з типом послуга створити поля Майстер, який може надавати цю послугу /
+    done - В sale.order.line створити поля Майстер, Дата надання послуги /
+    done - action_confirm перевіряє accessory_product_ids та додає їх до SO /
+    done - action_confirm викликає візард створення інвойсу /
+    done - action_confirm створює подію в календарях майстрів /
+    done - В sale.order.line додано посилання на календар майстрів /
+    done - calendar.event створено кнопку 'DONE' що створює таймшит у конкретного майстра /
+    """,
     'summary': 'Tools',
     'category': 'Tools',
     'website': 'http://www.simbioz.ua',
